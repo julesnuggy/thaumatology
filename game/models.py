bcf1e890d87e160a685fd69db18609ccafac3dfe
Julian Ng <julesnuggy@gmail.com>
2021-09-14 18:29:04 +0200

    Create first Game models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    type = models.CharField(max_length=20)
+    hp = models.IntegerField(default=100)
+    mp = models.IntegerField(default=100)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=20)
+    score = models.IntegerField(default=0)
+    character = models.ManyToManyField(Character)
+
+
+class Round(models.Model):
+    date = models.DateTimeField('game date')
+    round_number = models.IntegerField(default=1)
+    p1 = models.ManyToManyField(Player, related_name='round_p1')
+    p2 = models.ManyToManyField(Player, related_name='round_p2')
