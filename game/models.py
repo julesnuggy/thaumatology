db169cca29ca69b96fcbcd4903d5ebcaa1541d36
Julian Ng <julesnuggy@gmail.com>
2021-09-14 19:47:24 +0200

    Add str() methods to models

--- game/models.py
+++ game/models.py
@@ -6,11 +6,17 @@ class Character(models.Model):
     hp = models.IntegerField(default=100)
     mp = models.IntegerField(default=100)
 
+    def __str__(self):
+        return self.type
+
 
 class Player(models.Model):
     name = models.CharField(max_length=20)
     score = models.IntegerField(default=0)
     character = models.ManyToManyField(Character)
+
+    def __str__(self):
+        return self.name
 
 
 class Round(models.Model):
@@ -18,3 +24,7 @@ class Round(models.Model):
     round_number = models.IntegerField(default=1)
     p1 = models.ManyToManyField(Player, related_name='round_p1')
     p2 = models.ManyToManyField(Player, related_name='round_p2')
+
+    def __str__(self):
+        return f'{self.round_number}: {self.p1} vs {self.p2}'
+
