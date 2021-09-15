58ce005441e17609a0bab0263e60f121092f8c73
Julian Ng <julesnuggy@gmail.com>
2021-09-15 11:00:58 +0200

    Add Character views

--- game/urls.py
+++ game/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('characters', views.characters, name='characters'),
+    path('get_character/<int:character_id>', views.get_character, name='get_character'),
 ]
--- game/views.py
+++ game/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
 
+from game.models import Character
+
+
 def index(request):
     return HttpResponse('Welcome to Thaumatology!')
+
+
+def characters(request):
+    all_characters = ', '.join([c.type for c in Character.objects.all()])
+    return HttpResponse(f'{all_characters}')
+
+
+def get_character(request, character_id):
+    character = Character.objects.get(id=character_id)
+    return HttpResponse(f'Showing character: {character}')
